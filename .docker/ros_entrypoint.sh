6f6418d72330b23b571e00f57163da3664cc8ede
Sebastian Jahr <sebastian.jahr@picknik.ai>
2025-01-02 12:54:39 +0100

    Install Pinocchio + update pre-format

--- .docker/ros_entrypoint.sh
+++ .docker/ros_entrypoint.sh
@@ -7,5 +7,12 @@ source "${WORKSPACE}/install/setup.bash" --
 # Ensure pre-commit can be run from inside the container
 git config --global --add safe.directory "${WORKSPACE}/src/krakel"
 
+# Setup Pinocchio environment
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
+export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
+
 # Execute docker command
 exec "$@"
